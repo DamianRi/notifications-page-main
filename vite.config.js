6e018fe07a41c0aa473f851d097b8ec170a30b03
DamianRi <dams_rive@hotmail.com>
2022-11-01 01:25:30 -0600

    Fixing publicDir

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  publicDir: process.env.NODE_ENV === "production" ? "/notifications-page-main" : "/"
 })
