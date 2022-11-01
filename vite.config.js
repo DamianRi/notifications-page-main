f28a63085cf12f62282ca1a876ce0e47554ba58a
DamianRi <dams_rive@hotmail.com>
2022-11-01 01:34:37 -0600

    Fixing vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicDir: process.env.NODE_ENV === "production" ? "/notifications-page-main" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/notifications-page-main/" : "/",
+  build: {
+    outDir: './docs'
+  }
 })
